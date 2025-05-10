d53be9014d75310dfc1f97cecf1b6024fe79be0b
Richard Shuan <richard.shaun@outlook.com>
2025-05-10 13:47:02 +0800

    Improve the main entrance of gui. Fix the include path of application.h.

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -2,24 +2,23 @@
 * @file main.cpp
  * @brief MetaWiper GUI application entry point
  */
-#include <QApplication>  // Changed from QGuiApplication to QApplication
+#include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
-#include <QtQuickControls2/QQuickStyle>
+#include <QQuickStyle>
 #include "application.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
+
     // Enable high DPI support
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
 
     // Use QApplication instead of QGuiApplication to support widgets like QFileDialog
     QApplication app(argc, argv);
 
     // Set application information
-    app.setOrganizationName("MetaWiper");
-    app.setOrganizationDomain("metawiper.org");
+    app.setOrganizationName("Richard Shaun");
+    app.setOrganizationDomain("https://github.com/Richard-Shaun/");
     app.setApplicationName("MetaWiper");
     app.setApplicationVersion("0.1.0");
 
@@ -27,30 +26,34 @@ int main(int argc, char *argv[])
     QQuickStyle::setStyle("Material");
 
     // Create application instance
-    Application metaWiperApp;
+    Application meta_wiper_gui_app;
 
     // Set up QML engine
-    QQmlApplicationEngine engine;
+    QQmlApplicationEngine qml_engine;
 
     // Expose application instance to QML
-    engine.rootContext()->setContextProperty("app", &metaWiperApp);
+    qml_engine.rootContext()->setContextProperty("app", &meta_wiper_gui_app);
 
     // Also expose models directly to QML context to avoid calling getters
-    engine.rootContext()->setContextProperty("fileListModel", metaWiperApp.getFileListModel());
-    engine.rootContext()->setContextProperty("metadataModel", metaWiperApp.getMetadataModel());
-    engine.rootContext()->setContextProperty("mainViewModel", metaWiperApp.getMainViewModel());
+    qml_engine.rootContext()->setContextProperty("fileListModel", meta_wiper_gui_app.getFileListModel());
+    qml_engine.rootContext()->setContextProperty("metadataModel", meta_wiper_gui_app.getMetadataModel());
+    qml_engine.rootContext()->setContextProperty("mainViewModel", meta_wiper_gui_app.getMainViewModel());
 
-    // Register view models to QML - removed parameter
-    metaWiperApp.registerViewModels();
+    // Register view models to QML
+    meta_wiper_gui_app.registerViewModels();
 
     // Load main QML file
     const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+
+    QObject::connect(&qml_engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
+        if (!obj && url == objUrl) {
+            qCritical() << "Failed to load QML file:" << url.toString();
             QCoreApplication::exit(-1);
+        }
     }, Qt::QueuedConnection);
-    engine.load(url);
+
+    qml_engine.load(url);
 
     return app.exec();
 }
